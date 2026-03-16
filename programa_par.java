a6327de3dfa2065ff7b1862a13ab777b6dedda0c
Joel Y <yhjoel@gmail.com>
2026-03-16 11:21:34 -0400

    se modifico el programa que verifica el numero par

--- programa_par.java
+++ programa_par.java
@@ -1,12 +1,10 @@
 public class programa_par {
     public static void main(String[] args) {
-        int numero = 4; // Número a verificar
-
-        // Comprobación de paridad
+        int numero = 4;
         if (numero % 2 == 0) {
-            System.out.println(numero + " es par.");
+            System.out.println(numero + " es par");
         } else {
-            System.out.println(numero + " es impar.");
+            System.out.println(numero + " es impar");
         }
     }
 }
